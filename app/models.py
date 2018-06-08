7e754cb88e9a2b0ec49c385a4d322f3c2d441f68
Tushar S <tsaxena86@gmail.com>
2018-06-08 02:12:20 -0400

    add run.py and add burger model (not complete yet)

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from app import db
+
+class Burger(db.Model):
+    """This class represents the burger table."""
+
+    __tablename__ = 'burgers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, name):
+        """Initialize with name."""
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Burger.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Burger: {}>".format(self.name)
