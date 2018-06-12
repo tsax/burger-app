053e84b473e518e9433d308f323804a6a5d13c6b
Tushar S <tsaxena86@gmail.com>
2018-06-12 02:34:36 -0400

    one to many: burger has many toppings

--- app/models.py
+++ app/models.py
@@ -1,18 +1,20 @@
 from app import db
+from sqlalchemy.sql import expression
 
 class Burger(db.Model):
-    """This class represents the burger table."""
+    """This class represents the burgers table."""
 
     __tablename__ = 'burgers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    has_bun = db.Column(db.Boolean(create_constraint=True))
-    has_patty = db.Column(db.Boolean(create_constraint=True))
+    name = db.Column(db.String(255), nullable=False)
+    has_bun = db.Column(db.Boolean(create_constraint=True), server_default=expression.true())
+    has_patty = db.Column(db.Boolean(create_constraint=True), server_default=expression.true())
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
+    toppings = db.relationship('Topping', backref='burger', lazy=True)
 
     def __init__(self, name):
         self.name = name
@@ -31,3 +33,12 @@ class Burger(db.Model):
 
     def __repr__(self):
         return "<Burger: {}>".format(self.name)
+
+class Topping(db.Model):
+    """This class represents the toppings table."""
+
+    __tablename__ = 'toppings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    burger_id = db.Column(db.Integer, db.ForeignKey('burgers.id'), nullable=False)
